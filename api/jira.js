1fba3807930b8fca163f2fd36d62c3780b4defce
kaio-ferreira-DMX <kaio.ferreira@dmxcapital.com.br>
2026-03-30 16:28:55 -0300

    first commit

---
+++ api/jira.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  try {
+    const issue = req.body.issue;
+
+    const message = {
+      content: `🆕 ${issue.key} - ${issue.fields.summary}`
+    };
+
+    await fetch(process.env.DISCORD_WEBHOOK, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(message)
+    });
+
+    return res.status(200).json({ ok: true });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Erro interno" });
+  }
+}
